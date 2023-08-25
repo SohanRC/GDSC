33c0ce03279c0c025be4d1178db47e87b29f3e78
SOHAN <sohanroychowdhury7@gmail.com>
2023-08-25 19:51:52 +0530

    This is my First Commit

---
+++ Home Page/script.js
@@ -0,0 +1,15 @@
+// var i = 0;
+// var txt = "JGEC";
+// var speed = 500;
+// var p = document.querySelector('.typing');
+// const typeWriter = ()=> {
+//     if(i < txt.length) {
+//         p.innerHTML+= txt[i];
+//         i++;
+//     }
+//     else{
+//         i = 0;
+//         p.innerHTML = "";
+//     }    
+// }
+// setInterval(typeWriter , speed);
